478f2534c97bf45455ca5577a4cd75964b092659
Dan Hope <dplhope@gmail.com>
2021-12-11 15:30:22 +0000

    all katas complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+   const names = namesObj.map(nameObject => nameObject.name ) 
+    if(names.length > 1) {
+      const lastName = names.pop()
+     return (names.join(', ')+' & '+lastName)
+     }
+     return names[0];
+   };
 
 module.exports = joinNames;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,13 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it('returns the stirng Yes when passed as true', () => {
+
+  expect(booleanToWord(true)).toBe('Yes')
+  });
+
+  it('returns the string No when passed as false', () => {
+
+    expect(booleanToWord(false)).toBe('No')
+  })
 });
